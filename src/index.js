c012603287289f2a01f78c4ab4add7ced22460b5
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2021-10-26 21:18:24 +0100

    Add hook and use it in demo project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('')
+import { useState, useLayoutEffect } from 'react'
+
+export default function useIsRtl() {
+  const [isRtl, setIsRtl] = useState(false)
+  useLayoutEffect(() => {
+    setIsRtl(document.dir === 'rtl')
+  }, [document.dir])
+  return isRtl
+}

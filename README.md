06567fe67699cde2f607e5fe3d0f2cc11254e6cf
José Antonio González Rodríguez <jagonzalr@users.noreply.github.com>
2021-10-26 21:24:37 +0100

    Add useIsRtl hook (#2)
    * Add hook and use it in demo project
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
+[![npm version](https://badge.fury.io/js/use-is-rtl.svg)](https://badge.fury.io/js/use-is-rtl)
+
 # useIsRtl
+
 Determine text directionality of document.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Demo](#demo)
+- [License](#license)
+
+## Getting started
+
+`npm install --save use-is-rtl`
+
+## Usage
+
+```js
+const isRtl = useIsRtl()
+```
+
+Full example can be found [here](https://github.com/jagonzalr/useIsRtl/blob/main/demo/App.jsx)
+
+## Demo
+
+```
+git clone git@github.com:jagonzalr/useIsRtl.git
+cd useIsRtl
+npm intall
+npm start
+```
+
+## License
+
+useDarkTheme is [MIT licensed](./LICENSE).
--- demo/App.jsx
+++ demo/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
+
+import useIsRtl from '../src/index'
+
 import './styles/tailwind.css'
 import './styles/index.scss'
 
 export default () => {
+  const isRtl = useIsRtl()
+
+  const [dir, setDir] = useState(isRtl ? 'rtl' : 'ltr')
+
+  function changeDirection(e) {
+    e.preventDefault()
+    const newDir = isRtl ? 'ltr' : 'rtl'
+    document.dir = newDir
+    setDir(newDir)
+  }
+
   return (
     <div className='flex h-screen'>
       <div className='m-auto'>
         <h1 className='font-bold text-3xl flex-1'>useIsRtl</h1>
+        <p className='mb-2'>{isRtl ? 'Right to lefft' : 'Left to right'}</p>
+        <button className='bg-blue px-5 py-2' onClick={changeDirection}>
+          {`Change direction to ${dir === 'rtl' ? 'ltr' : 'rtl'}`}
+        </button>
       </div>
     </div>
   )

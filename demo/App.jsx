012340bdce8efd21b8890427a2735dd42d84ce4a
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2021-10-26 20:45:56 +0100

    Add npm deps and demo project

---
+++ demo/App.jsx
@@ -0,0 +1,12 @@
+import './styles/tailwind.css'
+import './styles/index.scss'
+
+export default () => {
+  return (
+    <div className='flex h-screen'>
+      <div className='m-auto'>
+        <h1 className='font-bold text-3xl flex-1'>useIsRtl</h1>
+      </div>
+    </div>
+  )
+}
